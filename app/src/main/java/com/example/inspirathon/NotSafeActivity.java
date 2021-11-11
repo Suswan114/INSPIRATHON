da9796d20fef22765ed692221dd9983c8e9af8c2
quenti20 <avikpat2000@gmail.com>
2021-11-11 19:02:14 +0530

    update1

--- app/src/main/java/com/example/inspirathon/NotSafeActivity.java
+++ app/src/main/java/com/example/inspirathon/NotSafeActivity.java
@@ -3,6 +3,7 @@ package com.example.inspirathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class NotSafeActivity extends AppCompatActivity {
@@ -13,6 +14,9 @@ public class NotSafeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_not_safe);
 
         Spinner mySpinner = (Spinner)findViewById(R.id.spinner);
-
+        ArrayAdapter<String>myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+         mySpinner.setAdapter(myAdapter);
     }
 }
