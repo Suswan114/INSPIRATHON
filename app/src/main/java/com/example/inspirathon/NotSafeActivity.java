c2dc3185fa626d32d883fe3f78e70f86f9a484b1
quenti20 <avikpat2000@gmail.com>
2021-11-11 18:03:33 +0530

    Updated Link Updating safety protocols

--- app/src/main/java/com/example/inspirathon/NotSafeActivity.java
+++ app/src/main/java/com/example/inspirathon/NotSafeActivity.java
@@ -3,6 +3,7 @@ package com.example.inspirathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class NotSafeActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class NotSafeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_not_safe);
+
+        Spinner mySpinner = (Spinner)findViewById(R.id.spinner);
+
     }
 }
