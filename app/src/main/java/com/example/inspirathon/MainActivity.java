b262988e9a3f14447202928ef8b65278aa030ff3
Suswan114 <infinityplaycons04@gmail.com>
2021-11-11 10:47:03 +0530

    4 Activities Added

--- app/src/main/java/com/example/inspirathon/MainActivity.java
+++ app/src/main/java/com/example/inspirathon/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.inspirathon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CardView notSAfeButton=(CardView) findViewById(R.id.notSafe);
+        CardView dangerButton=(CardView) findViewById(R.id.danger);
+        CardView linkButton=(CardView) findViewById(R.id.links);
+        CardView settingsButton=(CardView) findViewById(R.id.settings);
+        notSAfeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Call NOT SAFE ACTIVITY
+            }
+        });
+        dangerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Call DANGER ACTIVITY
+            }
+        });
+        linkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Call LINKS ACTIVITY
+            }
+        });
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Call SETTINGS ACTIVITY
+            }
+        });
     }
 }
